1f0d86d641a33340afe45f0b521fe4ad72f6e1cc
JovanaTrajkovic <trajkovicjovana369@gmail.com>
2023-08-29 18:23:15 +0200

    Komponenta Agencija

--- src/components/Agencija.jsx
+++ src/components/Agencija.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 
 
 
-function Agencija() {
+function Agencija(props) {
    
   return (
-    <div>
-      
+    <div className="col">
+    <div className="card">
+        <div className="row g-0">
+            <div className="col-5 col-sm-4">
+                <img className="img-fluid w-10"
+                 src="../slike/slika1.png" 
+                 alt='Lab' /> 
+            </div>
+            <div className="col-7 col-sm-8">
+                <div className="card-body">
+                    <h2 className="card-title">{props.agencija.naziv}</h2>
+                    <h4 className="card-text">{props.agencija.adresa}</h4>
+                    <br/>
+                    <p className="card-text"> <b>E-mail:</b> {props.agencija.email}</p>
+                    <p className="card-text"><b>Website:</b> {props.agencija.website}</p> 
+                    <p className="card-text"><b>Website:</b> {props.agencija.brojTelefona}</p> 
+                </div>
+            </div>
+        </div>
     </div>
-  )
+</div>
+)
+
 }
 
 export default Agencija
